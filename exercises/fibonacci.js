82a92f6f3aa9d192af4754119c86d5d34a4435fe
SereneAngelina <sereneangelina1@gmail.com>
2018-01-03 22:29:16 +0100

    answer

--- exercises/fibonacci.js
+++ exercises/fibonacci.js
@@ -1,4 +1,4 @@
-// 
+//
 // Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
 //
 // The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers. The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
@@ -10,3 +10,26 @@
 // Here are some helpful links:
 //
 // Remainder
+
+//Answer:
+
+function sumFibs(num) {
+   var a = 0;
+    var b = 1;
+
+    var result = 0;
+    while (b <= num) {
+      var c=a+b;
+        if (b % 2 !== 0) {
+            result += b;
+        }
+        a=b;
+        b = c;
+
+    }
+
+    return result;
+}
+
+
+sumFibs(4);
